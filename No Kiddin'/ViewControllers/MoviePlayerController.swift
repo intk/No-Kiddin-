7fe5a660873c3a2fb2f8a7d95bcbf2a7ccf4ef4d
Sjors Snoeren <hello@sjors.me>
2015-07-06 13:38:45 +0200

    Images are scale to fit and placed in a zoomable UIScrollView

--- No Kiddin'/ViewControllers/MoviePlayerController.swift
+++ No Kiddin'/ViewControllers/MoviePlayerController.swift
@@ -8,14 +8,16 @@
 
 import MediaPlayer
 
-class MoviePlayerController: MPMoviePlayerController {
+class MoviePlayerController: MPMoviePlayerController, UIScrollViewDelegate {
     
+    private var overlayScrollView: UIScrollView?
     private var overlayImageView: UIImageView?
+    
     private var displayAdjustmentView: UIView?
     
     internal var model: Art? {
         didSet {
-            loadImage()
+            overlayImageView?.image = model!.portraitArtImage
             
             if model!.displayMode == .Light {
                 displayAdjustmentView?.backgroundColor = UIColor(white: 0, alpha: 0.1)
@@ -28,32 +30,58 @@ class MoviePlayerController: MPMoviePlayerController {
     override init!(contentURL url: NSURL!) {
         super.init(contentURL: url)
         
+        overlayScrollView = UIScrollView()
+        overlayScrollView!.delegate = self
+        overlayScrollView!.minimumZoomScale = 1.0
+        overlayScrollView!.maximumZoomScale = 6.0
+        overlayScrollView!.backgroundColor = UIColor.blackColor()
+        view.addSubview(overlayScrollView!)
+        
         overlayImageView = UIImageView()
-        overlayImageView!.backgroundColor = UIColor.blackColor()
+        overlayImageView!.backgroundColor = UIColor.clearColor()
         overlayImageView!.clipsToBounds = true
-        view.addSubview(overlayImageView!)
+        overlayImageView!.contentMode = UIViewContentMode.ScaleAspectFit
+        overlayScrollView!.addSubview(overlayImageView!)
         
         displayAdjustmentView = UIView()
         displayAdjustmentView!.backgroundColor = UIColor(white: 0, alpha: 0.1)
+        displayAdjustmentView!.userInteractionEnabled = false
         view.addSubview(displayAdjustmentView!)
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-//        overlayImageView?.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height - 44)
-        overlayImageView?.frame = view.bounds
+
+        if let overlayScrollView = overlayScrollView {
+            overlayScrollView.frame = view.bounds
+            overlayScrollView.contentSize = overlayScrollView.frame.size
+            overlayImageView?.frame = overlayScrollView.bounds
+        }
+        
         displayAdjustmentView?.frame = view.bounds
-        
-        loadImage()
-        
     }
     
-    private func loadImage() {
-        if overlayImageView?.bounds.width < overlayImageView?.bounds.height {
-            overlayImageView?.image = model!.portraitArtImage
-        } else {
-            overlayImageView?.image = model!.landscapeArtImage
+    // MARK: - UIScrollViewDelegate
+    
+    internal func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return overlayImageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        var top: CGFloat = 0.0
+        var left: CGFloat = 0.0
+        
+        if overlayScrollView!.contentSize.width < overlayScrollView!.bounds.size.width {
+            left = (overlayScrollView!.bounds.size.width - overlayScrollView!.contentSize.width) * 0.5;
         }
+        if (overlayScrollView!.contentSize.height < overlayScrollView!.bounds.size.height) {
+            top = (overlayScrollView!.bounds.size.height-overlayScrollView!.contentSize.height) * 0.5;
+        }
+        overlayScrollView!.contentInset = UIEdgeInsetsMake(top, left, top, left);
+    }
+    
+    internal func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView!, atScale scale: CGFloat) {
+        
     }
     
 }
